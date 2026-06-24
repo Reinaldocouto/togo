a34409d719425ed79c6f309200528e271f53aa49
Reinaldo Couto <43384617+Reinaldocouto@users.noreply.github.com>
2026-06-24 15:58:52 -0300

    Fix prescription controller test routing import

--- backend/src/Togo.Api.Tests/Controllers/PrescriptionsControllerTests.cs
+++ backend/src/Togo.Api.Tests/Controllers/PrescriptionsControllerTests.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
